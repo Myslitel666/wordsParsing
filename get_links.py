57e856a8204290903987009baec2d076291b950b
Myslitel666 <tretyakov_02@bk.ru>
2026-08-23 22:00:36 +0500

    q

--- get_links.py
+++ get_links.py
@@ -83,13 +83,17 @@ def extract_links_from_url(url, base_url=None):
 
 
 # ═══════════════════════════════════════════════
-# 🔥 ЦИКЛ ПО СТРАНИЦАМ: 6 – 9 физика пройден, ИТ 1-3, МАТ 1-3, ХИМ 1-3, ПРАВО 1-3
+# 🔥 ЦИКЛ ПО СТРАНИЦАМ: 6 – 9 физика пройден, ИТ 1-3, МАТ 1-3, ХИМ 1-3, ПРАВО 1-3, ИСТОРИЯ 1-8
 # ═══════════════════════════════════════════════
 URLS_TO_PARSE = [
-    'https://cyberleninka.ru/article/c/history-and-archaeology/5',
-    'https://cyberleninka.ru/article/c/history-and-archaeology/6',
-    'https://cyberleninka.ru/article/c/history-and-archaeology/7',
-    'https://cyberleninka.ru/article/c/history-and-archaeology/8',
+    'https://cyberleninka.ru/article/c/history-and-archaeology/9',
+    'https://cyberleninka.ru/article/c/history-and-archaeology/10',
+    'https://cyberleninka.ru/article/c/history-and-archaeology/11',
+    'https://cyberleninka.ru/article/c/history-and-archaeology/12',
+    'https://cyberleninka.ru/article/c/history-and-archaeology/13',
+    'https://cyberleninka.ru/article/c/history-and-archaeology/14',
+    'https://cyberleninka.ru/article/c/history-and-archaeology/15',
+    'https://cyberleninka.ru/article/c/history-and-archaeology/16',    
 ]
 
 def get_links():
