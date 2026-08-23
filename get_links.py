f290fd8b5558a7177e790ddeec50b940b2756886
Myslitel666 <tretyakov_02@bk.ru>
2026-08-23 22:15:16 +0500

    q

--- get_links.py
+++ get_links.py
@@ -86,14 +86,7 @@ def extract_links_from_url(url, base_url=None):
 # 🔥 ЦИКЛ ПО СТРАНИЦАМ: 6 – 9 физика пройден, ИТ 1-3, МАТ 1-3, ХИМ 1-3, ПРАВО 1-3, ИСТОРИЯ 1-8
 # ═══════════════════════════════════════════════
 URLS_TO_PARSE = [
-    'https://cyberleninka.ru/article/c/history-and-archaeology/9',
-    'https://cyberleninka.ru/article/c/history-and-archaeology/10',
-    'https://cyberleninka.ru/article/c/history-and-archaeology/11',
-    'https://cyberleninka.ru/article/c/history-and-archaeology/12',
-    'https://cyberleninka.ru/article/c/history-and-archaeology/13',
-    'https://cyberleninka.ru/article/c/history-and-archaeology/14',
-    'https://cyberleninka.ru/article/c/history-and-archaeology/15',
-    'https://cyberleninka.ru/article/c/history-and-archaeology/16',    
+    'https://godsbay.ru/orient/', 
 ]
 
 def get_links():
