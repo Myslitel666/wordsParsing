772eefa725d12536c230a0664b6cd6c14c68e06c
Myslitel666 <tretyakov_02@bk.ru>
2026-08-30 08:24:09 +0500

    q

--- get_links.py
+++ get_links.py
@@ -83,12 +83,12 @@ def extract_links_from_url(url, base_url=None):
 
 
 # ═══════════════════════════════════════════════
-# 🔥 ЦИКЛ ПО СТРАНИЦАМ: 6 – 9 физика пройден, ИТ 59, МАТ 1-3, ХИМ 1-3, ПРАВО 1-3, ИСТОРИЯ 1-8
+# 🔥 ЦИКЛ ПО СТРАНИЦАМ: 6 – 9 (1 - 9) физика пройден, ИТ 59 (1 - 60), МАТ 1-3, ХИМ 1-3, ПРАВО 1-3, ИСТОРИЯ 1-8
 # ═══════════════════════════════════════════════
 URLS_TO_PARSE = [
     #'http://az.lib.ru/t/tolstoj_lew_nikolaewich/'
     f'https://cyberleninka.ru/article/c/computer-and-information-sciences/{i}'
-    for i in range(1, 9)
+    for i in range(30, 60)
 ]
 
 def get_links():
